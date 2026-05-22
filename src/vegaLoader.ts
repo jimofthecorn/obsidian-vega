ec5ddf77084aef412b0d13cfc14133b26a852e12
Jim Crowell <jacrowell@mailup.net>
2026-05-22 16:27:30 -0400

    ok, lot of unnecessary work, just needed to toggle an obsidian setting

--- src/vegaLoader.ts
+++ src/vegaLoader.ts
@@ -1,7 +1,6 @@
-import { normalizePath, TFile, MarkdownView, Notice } from "obsidian";
+import { normalizePath, TFile, Notice } from "obsidian";
 import { Loader } from "vega";
 import VegaVisualizationsPlugin from "./main";
-
 
 export default class VegaLoader implements Loader {
     private plugin: VegaVisualizationsPlugin;
@@ -48,18 +47,14 @@ export default class VegaLoader implements Loader {
 
     public async file(filename: string, _?: any): Promise<string> {
         const vault = this.plugin.app.vault;
-        const activeFile = this.plugin.app.workspace.getActiveFile();
-        // const activeView = this.plugin.app.workspace.getActiveViewOfType(MarkdownView);
+        const file = vault.getAbstractFileByPath(filename);
 
-        if (!activeFile) {
-            throw Error(`Problem finding "${filename}": active file is null`);
+        if (!(file instanceof TFile)) {
+			new Notice(`"${filename}" not found`);
+            // throw Error(`URL "${filename}" is not a file or does not exist`);
         }
-        const activeFolder = activeFile.parent.path;
-        const dataFile = vault.getAbstractFileByPath(filename);
-        if (!(dataFile instanceof TFile)) {
-            throw Error(`Problem finding "${filename}": "${dataFile}" is not a TFile (activeFolder = "${activeFolder}")`);
-        }
-        const result = await vault.cachedRead(dataFile);
+
+        const result = await vault.cachedRead(file);
         return result;
     }
 }
